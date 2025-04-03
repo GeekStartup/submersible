11e773940ef2ce7fb30f2cda7c4336345110d4e3
ashishk <ashishk@maveric-syatems.com>
2025-04-03 14:28:05 +0530

    GridServiceTest for create grid

---
+++ src/test/java/com/ocean/submersible/service/GridServiceTest.java
@@ -0,0 +1,49 @@
+package com.ocean.submersible.service;
+
+import com.ocean.submersible.entities.Grid;
+import com.ocean.submersible.entities.Obstacle;
+import com.ocean.submersible.repositories.GridRepository;
+import com.ocean.submersible.repositories.ObstacleRepository;
+import com.ocean.submersible.service.impl.GridService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class) // Automatically initializes mocks
+public class GridServiceTest {
+
+    @Mock
+    private GridRepository gridRepository;
+
+    @Mock
+    private ObstacleRepository obstacleRepository;
+
+    @InjectMocks
+    private GridService gridService;
+
+    private Grid mockGrid;
+    private Obstacle mockObstacle;
+
+    @BeforeEach
+    void setUp() {
+        mockGrid = Grid.builder().id(1L).width(10).height(10).build();
+        mockObstacle = Obstacle.builder().id(1L).x(3).y(4).grid(mockGrid).build();
+    }
+
+    @Test
+    void testCreateGrid() {
+        when(gridRepository.save(any(Grid.class))).thenReturn(mockGrid);
+        Grid createdGrid = gridService.createGrid(10, 10);
+        assertNotNull(createdGrid);
+        assertEquals(10, createdGrid.getWidth());
+        assertEquals(10, createdGrid.getHeight());
+    }
+}
