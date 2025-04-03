2829b68789a7a3a93774c3bd42a927a1f6eb5a05
ashishk <ashishk@maveric-syatems.com>
2025-04-03 17:44:57 +0530

    Adding controllers for probe and grid

---
+++ src/main/java/com/ocean/submersible/controller/GridController.java
@@ -0,0 +1,43 @@
+package com.ocean.submersible.controller;
+
+
+import com.ocean.submersible.entities.Grid;
+import com.ocean.submersible.entities.Obstacle;
+import com.ocean.submersible.service.impl.GridService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/grid")
+public class GridController {
+
+    private final GridService gridService;
+
+    @PostMapping("/create")
+    public Grid createGrid(@RequestParam int width, @RequestParam int height) {
+        return gridService.createGrid(width, height);
+    }
+
+    @PostMapping("/{gridId}/addObstacle")
+    public Obstacle addObstacle(@PathVariable Long gridId, @RequestParam int x, @RequestParam int y) {
+        return gridService.addObstacle(gridId, x, y);
+    }
+
+    @GetMapping("/{gridId}")
+    public Grid getGrid(@PathVariable Long gridId) {
+        return gridService.getGrid(gridId);
+    }
+
+    @GetMapping("/{gridId}/obstacles")
+    public List<Obstacle> getObstacles(@PathVariable Long gridId) {
+        return gridService.getObstacles(gridId);
+    }
+}
---
+++ src/main/java/com/ocean/submersible/controller/ProbeController.java
@@ -0,0 +1,51 @@
+package com.ocean.submersible.controller;
+
+import com.ocean.submersible.entities.Probe;
+import com.ocean.submersible.enums.Direction;
+import com.ocean.submersible.service.impl.ProbeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/probe")
+@RequiredArgsConstructor
+public class ProbeController {
+
+    private final ProbeService probeService;
+
+    @PostMapping("/create")
+    public Probe createProbe(@RequestParam Long gridId, @RequestParam int x, @RequestParam int y, @RequestParam Direction facingDirection) {
+        return probeService.createProbe(gridId, x, y, facingDirection);
+    }
+
+    @GetMapping("/{probeId}")
+    public Probe getProbe(@PathVariable Long probeId) {
+        return probeService.getProbe(probeId);
+    }
+
+    @PostMapping("/{probeId}/moveForward")
+    public Probe moveForward(@PathVariable Long probeId) {
+        return probeService.moveForward(probeId);
+    }
+
+    @PostMapping("/{probeId}/moveBackward")
+    public Probe moveBackward(@PathVariable Long probeId) {
+        return probeService.moveBackward(probeId);
+    }
+
+    @PostMapping("/{probeId}/turnLeft")
+    public Probe turnLeft(@PathVariable Long probeId) {
+        return probeService.turnLeft(probeId);
+    }
+
+    @PostMapping("/{probeId}/turnRight")
+    public Probe turnRight(@PathVariable Long probeId) {
+        return probeService.turnRight(probeId);
+    }
+
+}
